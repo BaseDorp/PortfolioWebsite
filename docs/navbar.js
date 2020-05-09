e2cb5e52a0ee9f131ef8ba7a9985297a6888cd9e
BaseDorp <44095612+BaseDorp@users.noreply.github.com>
2020-05-09 10:42:35 -0500

    Adds top and bottom bar to js file. Adds animation for page content

--- docs/navbar.js
+++ docs/navbar.js
@@ -1,1 +1,3 @@
-$(".nav-bar").html("inserthtmlhere");
+$( ".nav-bar" ).append( "<div class='logo'>Sam Hirsch</div><div class='tabs'><a href='../index.html'> About </a><a href='../Programming/index.html'> Programming </a><a href='../3DModels/index.html'> 3D Models </a><a href='../Contact/index.html'> Contact </a></div>" );
+
+$(".bottom-bar").append("<a href='https://github.com/BaseDorp' target='_blank'>Github</a><a href='https://github.com/BaseDorp' target='_blank'>LinkedIn</a><a href='https://github.com/BaseDorp/PortfolioWebsite' target='_blank'>Website Source Code</a></div>")
