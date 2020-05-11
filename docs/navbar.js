ace8c3a0cf59fbd4b3f2cb87856a761f37b6976f
BaseDorp <44095612+BaseDorp@users.noreply.github.com>
2020-05-11 13:03:15 -0500

    updated filepaths

--- docs/navbar.js
+++ docs/navbar.js
@@ -1,3 +1,3 @@
-$( ".nav-bar" ).append( "<div class='logo'>Sam Hirsch</div><div class='tabs'><a href='about.html'> About </a><a href='Programming/index.html'> Programming </a><a href='3DModels/index.html'> 3D Models </a><a href='Contact/index.html'> Contact </a></div>" );
+$( ".nav-bar" ).append( "<div class='logo'>Sam Hirsch</div><div class='tabs'><a href='/'> About </a><a href='/Programming/index.html'> Programming </a><a href='/3DModels/index.html'> 3D Models </a><a href='/Contact/index.html'> Contact </a></div>" );
 
 $(".bottom-bar").append("<a href='https://github.com/BaseDorp' target='_blank'>Github</a><a href='https://github.com/BaseDorp' target='_blank'>LinkedIn</a><a href='https://github.com/BaseDorp/PortfolioWebsite' target='_blank'>Website Source Code</a></div>")
